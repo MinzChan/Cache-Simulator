ae44d227be4fb226a43538050f9f1ff2c09f0aec
wanrong <zhuwr@pku.edu.cn>
2017-12-14 09:17:54 +0800

    Too SLOW  to run locally...

--- Complex/run.sh
+++ Complex/run.sh
@@ -1,4 +1,5 @@
 g++ memory.cc cache.cc main.cc -o cache
-#./cache trace/2.trace
-./cache ../Simple/trace/1.trace
-./cache ../Simple/trace/2.trace
+./cache trace/1.trace
+./cache trace/2.trace
+#./cache ../Simple/trace/1.trace
+#./cache ../Simple/trace/2.trace
